22d1300601258fbe896899ec4b6675ba09f3da47
Guilherme Farias <guicfarias11@gmail.com>
2021-03-18 14:20:52 -0300

    🎨 Visual: NewTransactionModal Finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
-import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
+import logoImg from '../../assets/logo.svg'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="My Wallet" />
-        <button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
       </Content>
